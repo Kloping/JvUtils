27f73b33f5c2b95d2f11f84a649ccd9cbdf726a4
HRS-Computer <3474006766@qq.com>
2022-05-31 12:16:39 +0800

    0.4.3

--- src/main/java/io/github/kloping/map/MapUtils.java
+++ src/main/java/io/github/kloping/map/MapUtils.java
@@ -1,7 +1,9 @@
 package io.github.kloping.map;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -11,33 +13,71 @@ import static io.github.kloping.judge.Judge.isNotNull;
  * @author github-kloping
  */
 public class MapUtils {
+
     /**
-     * 向Map的K List 添加元素 避免 List 为空的情况
+     * 向Map的K Set 添加元素 避免 Set 为空的情况
+     *
      * @param map
-     * @param k map k
-     * @param v list v
+     * @param k   map k
+     * @param v   list v
      * @param <K>
      * @param <V>
      */
-    public static <K, V> void append(Map<K, List<V>> map, K k, V v) {
+    public static <K, V> void appendSet(Map<K, Set<V>> map, K k, V v) {
         if (!isNotNull(map, k, v)) return;
-        List<V> list = map.get(k);
-        if (list == null) list = new CopyOnWriteArrayList<>();
+        Set<V> list = map.get(k);
+        if (list == null) list = new HashSet<>();
         list.add(v);
         map.put(k, list);
     }
 
     /**
-     * 向Map的K List 添加元素 避免 List 为空的情况 且自定义 List 类型
+     * 向Map的K Set 添加元素 避免 Set 为空的情况 且自定义 Set 类型
      * @param map
-     * @param k map k
-     * @param v list v
+     * @param k
+     * @param v
+     * @param cla
+     * @param <K>
+     * @param <V>
+     */
+    public static <K, V> void appendSet(Map<K, Set<V>> map, K k, V v, Class<? extends Set> cla) {
+        try {
+            if (!isNotNull(map, k, v)) return;
+            Set<V> list = map.get(k);
+            if (list == null) list = cla.newInstance();
+            list.add(v);
+            map.put(k, list);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 向Map的K List 添加元素 避免 List 为空的情况
+     *
+     * @param map
+     * @param k   map k
+     * @param v   list v
+     * @param <K>
+     * @param <V>
+     */
+    public static <K, V> void append(Map<K, List<V>> map, K k, V v) {
+        append(map, k, v, CopyOnWriteArrayList.class);
+    }
+
+    /**
+     * 向Map的K List 添加元素 避免 List 为空的情况 且自定义 List 类型
+     *
+     * @param map
+     * @param k   map k
+     * @param v   list v
      * @param cla list type
      * @param <K>
      * @param <V>
-     * @param <T>
      */
-    public static <K, V, T extends List> void append(Map<K, List<V>> map, K k, V v, Class<T> cla) {
+    public static <K, V> void append(Map<K, List<V>> map, K k, V v, Class<? extends List> cla) {
         try {
             if (!isNotNull(map, k, v)) return;
             List<V> list = map.get(k);
@@ -51,35 +91,33 @@ public class MapUtils {
 
     /**
      * 向Map的K Map 添加键值 避免 Map 为null的情况
+     *
      * @param map
-     * @param mk map k
-     * @param k map V map K
-     * @param v map V map v
+     * @param mk   map k
+     * @param k    map V map K
+     * @param v    map V map v
      * @param <mK>
      * @param <K>
      * @param <V>
      */
     public static <mK, K, V> void append(Map<mK, Map<K, V>> map, mK mk, K k, V v) {
-        if (!isNotNull(map, mk, k, v)) return;
-        Map<K, V> m = map.get(mk);
-        if (m == null) m = new ConcurrentHashMap<>();
-        m.put(k, v);
-        map.put(mk, m);
+        append(map, mk, k, v, ConcurrentHashMap.class);
     }
 
     /**
      * 向Map的K Map 添加键值 避免 Map 为null的情况 且自定义 map 类型
+     *
      * @param map
-     * @param mk map k
-     * @param k map V map k
-     * @param v map V map v
+     * @param mk   map k
+     * @param k    map V map k
+     * @param v    map V map v
      * @param cla
      * @param <mK>
      * @param <K>
      * @param <V>
      * @param <T>
      */
-    public static <mK, K, V, T extends Map> void append(Map<mK, Map<K, V>> map, mK mk, K k, V v, Class<T> cla) {
+    public static <mK, K, V> void append(Map<mK, Map<K, V>> map, mK mk, K k, V v, Class<? extends Map> cla) {
         try {
             if (!isNotNull(map, mk, k, v)) return;
             Map<K, V> m = map.get(mk);
